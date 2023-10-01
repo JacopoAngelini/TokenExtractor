b8c16a31bb9e84679411473cadac89ceb508c742
Jacopo Angelini <jacopo.angelini@live.it>
2023-10-01 15:04:53 +0200

    estrazione con specifica di quanti token estrarre

--- script.js
+++ script.js
@@ -4,65 +4,78 @@ var blues = [];
 var reds = [];
 var total = [];
 var numeroEstratto;
+var numDaEstrarre;
+var tokenEstratti = [];
 
-function info(){
-    alert('1)Inserire il numero dei token in entrambi i campi\n2)Prima estrazione cliccare estrai\n3)Per estrazioni successive cliccare estrai di nuovo\n4)Per cambiare numero di token inserire i nuovi valori nei campi e cliccare estrai per ricominciare le estrazioni')
+function inserisciToken() {
+  document.getElementById("risultato").innerHTML = "";
+
+  tokenGiocatore = document.getElementById("tokenGiocatore").value;
+  tokenMaster = document.getElementById("tokenMaster").value;
+
+  blues.length = 0;
+  reds.length = 0;
+
+  for (let i = 0; i < tokenGiocatore; i++) {
+    blues.push("B");
+  }
+  for (let i = 0; i < tokenMaster; i++) {
+    reds.push("R");
+  }
+
+  total = reds.concat(blues);
+  document.getElementById("divNumTokenEstrarre").classList.remove("d-none")
 }
 
-function estraiDiNuovo() {
-    document.getElementById('risultato').innerHTML = '';
-    numeroEstratto = Math.floor(Math.random() * total.length);
-
-    if(numeroEstratto != undefined){
-        valutazioneToken();
-        console.log(total)
-    }
-    else{
-        document.getElementById('risultato').innerHTML += '<p>inserisci i numeri nei campi predecendi e usa prima il bottone etrai</p>';
-    }
-  
+function plus() {
+  numDaEstrarre = Number(document.getElementById("NumTokenEstrarre").innerHTML);
+  if (numDaEstrarre >= total.length) {
+    document.getElementById("NumTokenEstrarre").innerHTML = numDaEstrarre;
+  } else {
+    document.getElementById("NumTokenEstrarre").innerHTML = numDaEstrarre + 1;
+  }
 }
+
+function minus() {
+  numDaEstrarre = Number(document.getElementById("NumTokenEstrarre").innerHTML);
+  if (numDaEstrarre <= 0) {
+    document.getElementById("NumTokenEstrarre").innerHTML = 0;
+  } else {
+    document.getElementById("NumTokenEstrarre").innerHTML = numDaEstrarre - 1;
+  }
+}
+
+function estrai() {
+    numDaEstrarre = Number(document.getElementById("NumTokenEstrarre").innerHTML);
+    for (let i = 0; i < numDaEstrarre; i++) {
+        numeroEstratto = Math.floor(Math.random() * total.length);
+
+        console.log("totale prima dell estrazione " + total);
+      
+        valutazioneToken(); 
+    }
+    tokenEstratti.length = 0;
+}
+
 
 function valutazioneToken() {
-
-    if(total[numeroEstratto] == 'B') {
-        document.getElementById('risultato').innerHTML += '<p style="color:blue; font-weight:700">Il token estratto è: blu</p>';
-        blues.pop()
+    if (total[numeroEstratto] == "B") {
+      tokenEstratti.push('<span style="color:blue">Blu</span>')
+      blues.pop();
     }
-    if(total[numeroEstratto] == 'R') {
-        document.getElementById('risultato').innerHTML += '<p style="color:red; font-weight:700">Il token estratto è: rosso</p>';
-        reds.pop()
+    if (total[numeroEstratto] == "R") {
+      tokenEstratti.push('<span style="color:red">Rosso</span>')
+      reds.pop();
     }
-    if(total[numeroEstratto] == undefined){
-        document.getElementById('risultato').innerHTML += '<p style="color:brown; font-weight:700">Riempire i campi precedenti</p>';
-    }
+    document.getElementById("risultato").innerHTML = "<p>"+ tokenEstratti +"</p>";
     total.splice(numeroEstratto, 1);
-    console.log('totale dopo estrazione ' + total)
-    document.getElementById('risultato').innerHTML += '<p >Numero token rimanenti: '+ total.length +', di cui <span style="color:blue">'+ blues.length +' blu</span> e  <span style="color:red"> '+ reds.length +' rossi</span></p>'
-   
-}
-
-function estraiToken () {
-    document.getElementById('risultato').innerHTML = '';
-
-    tokenGiocatore = document.getElementById('tokenGiocatore').value;
-    tokenMaster = document.getElementById('tokenMaster').value;
-    
-    blues.length = 0;
-    reds.length = 0;
-
-    for (let i = 0; i < tokenGiocatore; i++) {
-        blues.push('B')
-    }
-    for (let i = 0; i < tokenMaster; i++) {
-        reds.push('R')
-    }
-
-    total = reds.concat(blues);
-    numeroEstratto = Math.floor(Math.random() * total.length);
-
-    console.log('totale prima dell estrazione ' + total)
-
-    valutazioneToken()
-
-}
+    console.log("totale dopo estrazione " + total);
+    document.getElementById("risultato").innerHTML +=
+      "<p >Numero token rimanenti: " +
+      total.length +
+      ', di cui <span style="color:blue">' +
+      blues.length +
+      ' blu</span> e  <span style="color:red"> ' +
+      reds.length +
+      " rossi</span></p>";
+  }
